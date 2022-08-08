08d74519cc1a1b27e654c1f82335fc90ae1544bc
sagar <sagarsagu515@gmail.com>
2022-08-08 21:48:21 +0530

    class 16

---
+++ WebGentle.BookStore/WebGentle.BookStore/Controllers/BookController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using WebGentle.BookStore.Model;
+using WebGentle.BookStore.Repository;
+
+namespace WebGentle.BookStore.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly BookRepository _bookRepository = null;
+
+        public BookController()
+        {
+            _bookRepository = new BookRepository();    
+        }
+        
+        public ViewResult GetAllBooks()
+        {
+            var data = _bookRepository.GetAllBooks();
+            return View();
+        }
+
+        public BookModel GetABook(int id)
+        {
+            return _bookRepository.GetABook(id);
+        }
+
+        public List<BookModel> SearchBooks(string boookName, string authourName)
+        {
+            return _bookRepository.SearchBook(boookName, authourName);
+        }
+    }
+}
---
+++ WebGentle.BookStore/WebGentle.BookStore/Repository/BookRepository.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using WebGentle.BookStore.Model;
+
+namespace WebGentle.BookStore.Repository
+{
+    public class BookRepository
+    {
+        public List<BookModel> GetAllBooks()
+        {
+            return DataSource();
+        }
+        public BookModel GetABook(int id)
+        {
+            return DataSource().Where(x => x.Id == id).FirstOrDefault();
+        }
+        public List<BookModel> SearchBook(string title, string authourname)
+        {
+            return DataSource().Where(x => x.Title.Contains(title) || x.Author.Contains(authourname)).ToList();
+        }
+
+        private List<BookModel> DataSource()
+        {
+            return new List<BookModel>()
+            {   
+                new BookModel(){Id = 1, Title = "title1", Author = "Authour1"},
+                new BookModel(){Id = 2, Title = "title2", Author = "Authour2"},
+                new BookModel(){Id = 3, Title = "title3", Author = "Authour3"},
+                new BookModel(){Id = 4, Title = "title4", Author = "Authour4"},
+                new BookModel(){Id = 5, Title = "title5", Author = "Authour5"},
+            };
+        }
+    }
+}
